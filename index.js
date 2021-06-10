54efb3f65d6d725c4724b64a8b8b7b0651153653
tuguldurbold <trae.bold@gmail.com>
2021-06-09 17:12:44 -0700

    Update index.js
    Added emoji for winner

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ if (randomNumber1 == randomNumber2) {
   document.querySelector("h1").innerHTML = "Draw!";
 }
 else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🔥Player 1 Wins!";
 }
 else {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🔥";
 }
